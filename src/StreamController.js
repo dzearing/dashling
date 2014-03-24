5005bcadd6df64146cf3a0afe5824b50770e5052
dzearing <dzearing@hotmail.com>
2014-03-24 09:08:18 -0700

    Didn't save.

--- src/StreamController.js
+++ src/StreamController.js
@@ -25,7 +25,7 @@ Dashling.StreamController = function(videoElement, mediaSource, settings) {
   var firstFragmentDuration = _this._audioStream.fragments[0].time.lengthSeconds;
 
   if (settings.startTime && firstFragmentDuration) {
-    this._appendIndex = Math.max(0, Math.min(_this._audioStream.fragments.length - 1, ((settings.startTime / _this._audioStream.fragments[0].lengthSeconds) - 1)));
+    this._appendIndex = Math.max(0, Math.min(_this._audioStream.fragments.length - 1, ((settings.startTime / firstFragmentDuration) - 1)));
   }
 
   _this._loadNextFragment();
