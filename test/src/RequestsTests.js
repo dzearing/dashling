aa2b09f932fe7aeadaed60053ee80ed2b7600966
dzearing <dzearing@hotmail.com>
2014-03-25 23:10:58 -0700

    Fixing unit tests.

--- test/src/RequestsTests.js
+++ test/src/RequestsTests.js
@@ -1,42 +1,53 @@
+var settings = {
+  maxRetries: 3,
+  delaysBetweenRetries: [0]
+
+};
+
 asyncTest("Request.load success", function() {
-    expect(4);
+  expect(4);
 
-    var requests = new Dashling.RequestManager();
-    var request = { url: "foo" };
-    var success = false;
+  var requests = new Dashling.RequestManager(false, settings);
 
-    requests.maxRetries = 3;
-    requests.delayBetweenRetries = [ 0 ];
-    requests._xhrType = MockXHR.mockTextResponse("success", 200, 20);
+  var request = {
+    url: "foo"
+  };
+  var success = false;
 
-    requests.load(request, false, function() {
-        ok(true, "Called success callback");
-        equal(request.data, "success", "Response data was correct")
-        equal(request.statusCode, 200, "Response status code was correct");
-        equal(request.state, Dashling.FragmentState.downloaded, "Response has correct dashling state");
+  requests.maxRetries = 3;
+  requests.delayBetweenRetries = [0];
+  requests._xhrType = MockXHR.mockTextResponse("success", 200, 20);
 
-        start();
-    });
+  requests.load(request, false, function() {
+    ok(true, "Called success callback");
+    equal(request.data, "success", "Response data was correct")
+    equal(request.statusCode, 200, "Response status code was correct");
+    equal(request.state, Dashling.FragmentState.downloaded, "Response has correct dashling state");
+
+    start();
+  });
 });
 
 asyncTest("Request.load failure", function() {
-    expect(3);
+  expect(3);
 
-    var requests = new Dashling.RequestManager();
-    var request = { url: "foo" };
-    var calledFailure = false;
+  var requests = new Dashling.RequestManager(false, settings);
 
-    requests.maxRetries = 3;
-    requests.delayBetweenRetries = [ 0 ];
-    requests._xhrType = MockXHR.mockTextResponse("fail", 500, 20);
+  var request = {
+    url: "foo"
+  };
+  var calledFailure = false;
 
-    requests.load(request, false, null, function() {
-        ok(true, "Called failure callback");
-        equal(request.statusCode, 500, "Response status code was correct");
-        equal(request.state, Dashling.FragmentState.error, "Response has correct dashling state");
+  requests.maxRetries = 3;
+  requests.delayBetweenRetries = [0];
+  requests._xhrType = MockXHR.mockTextResponse("fail", 500, 20);
 
-        start();
-     });
+  requests.load(request, false, null, function() {
+    ok(true, "Called failure callback");
+    equal(request.statusCode, 500, "Response status code was correct");
+    equal(request.state, Dashling.FragmentState.error, "Response has correct dashling state");
+
+    start();
+  });
 
 });
-
