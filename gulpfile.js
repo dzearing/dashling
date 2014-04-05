366179d54e1453487552e703b97ffc1a868ad35a
dzearing <dzearing@hotmail.com>
2014-04-04 17:13:24 -0700

    Adding coverage support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify")
-var rename = require("gulp-rename");
-var qunit = require("gulp-qunit");
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename');
+var karma = require('gulp-karma');
+var coveralls = require('gulp-coveralls');
 
 var paths = {
-  scripts: inDirectory("src/", [
-    "Start.js",
-    "Utilities.js",
-    "ThrottleMixin.js",
-    "EventingMixin.js",
-    "DashlingEnums.js",
-    "Dashling.js",
-    "Settings.js",
-    "ManifestParser.js",
-    "StreamController.js",
-    "Stream.js",
-    "RequestManager.js",
-    "End.js"
+  scripts: inDirectory('src/', [
+    'Utilities.js',
+    'ThrottleMixin.js',
+    'EventingMixin.js',
+    'DashlingEnums.js',
+    'Dashling.js',
+    'Settings.js',
+    'ManifestParser.js',
+    'StreamController.js',
+    'Stream.js',
+    'RequestManager.js'
   ]),
-  testHosts: ["test/*.html"],
-  testFiles: ["test/src/*.js"]
+  testFiles: ['test/src/*.js']
 };
+
+paths.wrappedScripts = ['src/Start.js'].concat(paths.scripts, ['src/End.js']);
 
 function inDirectory(dir, files) {
   var newSet = [];
@@ -33,29 +34,45 @@ function inDirectory(dir, files) {
   return newSet;
 }
 
-gulp.task("scripts", function() {
+gulp.task('scripts', function() {
 
-  gulp.src(paths.scripts)
-    .pipe(concat("dashling.full.js"))
-    .pipe(gulp.dest("./dist"))
+  gulp.src(paths.wrappedScripts)
+    .pipe(concat('dashling.full.js'))
+    .pipe(gulp.dest('./dist'))
     .pipe(uglify())
-    .pipe(rename("dashling.min.js"))
-    .pipe(gulp.dest("./dist"));
+    .pipe(rename('dashling.min.js'))
+    .pipe(gulp.dest('./dist'));
 
 });
 
+gulp.task('test', ['scripts'], function(cb) {
+  gulp.src('coverage')
+    .pipe(clean());
 
-gulp.task("tests", function() {
-  return gulp.src(paths.testHosts)
-    .pipe(qunit());
+  return gulp.src(paths.wrappedScripts.concat(paths.testFiles))
+    .pipe(karma({
+      configFile: 'karma.config.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('officialTest', ['scripts'], function() {
+  gulp.run('test', function() {
+    gulp.src('coverage/**/lcov.info')
+      .pipe(coveralls());
+  });
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ["scripts"]);
-  gulp.watch(paths.testFiles, ["tests"]);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.testFiles, ['test']);
 });
 
-gulp.task("default", ["scripts", "tests"], function() {
+gulp.task('default', ['scripts', 'test'], function() {
 
 });
