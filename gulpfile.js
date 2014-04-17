9896403af93b099aefaa6fe6b66272749015c835
David Zearing <dzearing@hotmail.com>
2014-04-17 13:23:34 -0700

    Adding a comment

--- gulpfile.js
+++ gulpfile.js
@@ -8,81 +8,82 @@ var coveralls = require('gulp-coveralls');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
+// These are my source files.
 var paths = {
-  scripts: inDirectory('src/', [
-    'Utilities.js',
-    'ThrottleMixin.js',
-    'EventingMixin.js',
-    'DashlingEnums.js',
-    'Dashling.js',
-    'Settings.js',
-    'ManifestParser.js',
-    'StreamController.js',
-    'Stream.js',
-    'RequestManager.js'
-  ]),
-  testFiles: ['test/src/*.js']
+    scripts: inDirectory('src/', [
+        'Utilities.js',
+        'ThrottleMixin.js',
+        'EventingMixin.js',
+        'DashlingEnums.js',
+        'Dashling.js',
+        'Settings.js',
+        'ManifestParser.js',
+        'StreamController.js',
+        'Stream.js',
+        'RequestManager.js'
+    ]),
+    testFiles: ['test/src/*.js']
 };
 
 paths.wrappedScripts = ['src/Start.js'].concat(paths.scripts, ['src/End.js']);
 
 function inDirectory(dir, files) {
-  var newSet = [];
+    var newSet = [];
 
-  for (var i = 0; i < files.length; i++) {
-    newSet.push(dir + files[i]);
-  }
+    for (var i = 0; i < files.length; i++) {
+        newSet.push(dir + files[i]);
+    }
 
-  return newSet;
+    return newSet;
 }
 
 gulp.task('clean', function() {
-  gulp.src(['coverage', 'dist'])
-    .pipe(clean());
+    gulp.src(['coverage', 'dist'])
+        .pipe(clean());
 });
 
 
 gulp.task('jshint', function() {
-  return gulp.src(paths.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'));
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('scripts', ['clean', 'jshint', 'testscripts'], function(cb) {
-  return gulp.src(paths.wrappedScripts)
-    .pipe(concat('dashling.full.js', {
-      newLine: '\r\n'
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(uglify())
-    .pipe(rename('dashling.min.js'))
-    .pipe(gulp.dest('dist'));
+    return gulp.src(paths.wrappedScripts)
+        .pipe(concat('dashling.full.js', {
+            newLine: '\r\n'
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename('dashling.min.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('testscripts', ['clean', 'jshint'], function() {
-  return gulp.src(paths.scripts)
-    .pipe(concat('dashling.test.js'))
-    .pipe(gulp.dest('dist'));
+    return gulp.src(paths.scripts)
+        .pipe(concat('dashling.test.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('test', ['clean', 'scripts'], function(cb) {
-  return gulp.src(paths.wrappedScripts.concat(paths.testFiles))
-    .pipe(karma({
-      configFile: 'karma.config.js',
-      action: 'run'
-    }));
+    return gulp.src(paths.wrappedScripts.concat(paths.testFiles))
+        .pipe(karma({
+            configFile: 'karma.config.js',
+            action: 'run'
+        }));
 });
 
 gulp.task('covertest', ['scripts', 'test'], function() {
-  gulp.src('coverage/**/lcov.info')
-    .pipe(coveralls());
+    gulp.src('coverage/**/lcov.info')
+        .pipe(coveralls());
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.testFiles, ['scripts']);
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.testFiles, ['scripts']);
 });
 
 gulp.task('default', ['jshint', 'scripts', 'test'], function() {
