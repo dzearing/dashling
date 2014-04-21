b2d2f4909847e45febc7e8282d6250c8f9d19b24
dzearing <dzearing@hotmail.com>
2014-04-20 20:25:32 -0700

    Changes

--- test/src/StreamTests.js
+++ test/src/StreamTests.js
@@ -26,3 +26,25 @@ test("Stream.constructor", function() {
 
   ok(stream.isDisposed, "Stream was disposed");
 });
+/*
+test("Stream ABR quality shifting", function() {
+  var stream = new Dashling.Stream("video", null, null, {});
+
+  equal(stream.qualityIndex, 2, "Defaults to the right quality");
+
+  equal(stream.qualityIndex, 0, "Quality downshift occurs immediately once we have bandwidth information");
+
+  equal(stream.qualityIndex, 0, "Quality upshift occurs immediately once we have bandwidth information");
+
+  equal(stream.qualityIndex, 0, "Quality doesn't upshift if we just switched qualities");
+
+  equal(stream.qualityIndex, 0, "Quality upshifts after min number of fragments have played the current quality");
+
+  equal(stream.qualityIndex, 0, "Quality can downshift even if buffer is insufficient");
+
+  equal(stream.qualityIndex, 0, "Increase in bandwidth can downshift even if buffer is insufficient");
+
+equal(stream.qualityIndex, 0, "")
+
+});
+*/
